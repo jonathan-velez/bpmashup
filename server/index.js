e50c83d687a36f333b2b418a2f22090010f8169b
Jonathan Velez <jonathanvelez@gmail.com>
2018-04-19 21:41:39 -0700

    procfile

--- server/index.js
+++ server/index.js
@@ -32,6 +32,8 @@ app.get('*', (req, res) => {
   res.send('{}');
 });
 
+app.use('/*', staticFiles)
+
 app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
