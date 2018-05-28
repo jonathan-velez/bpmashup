32eb7be373a457badfa60949a09baf165660530c
Jonathan Velez <jonathanvelez@gmail.com>
2018-05-28 14:27:49 -0700

    cursor pointer for header text

--- client/src/components/PlaylistHeader.js
+++ client/src/components/PlaylistHeader.js
@@ -5,7 +5,7 @@ const PlaylistHeader = ({ playlistName, editHeader, deletePlaylist }) => {
   return (
     <React.Fragment>
       <Header size='huge'>
-        <span onClick={editHeader}>{playlistName}</span>
+        <span onClick={editHeader} className='playlistHeader'>{playlistName}</span>
         &nbsp;
         <span><Icon name='delete' size='small' color='red' onClick={deletePlaylist} /></span>
       </Header>
