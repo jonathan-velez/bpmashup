2315c3b2f20c5655a872a5dbb650913327522edc
Jonathan Velez <jonathanvelez@gmail.com>
2018-05-29 20:02:34 -0700

    added animation

--- client/src/components/TrackListingTableBody.js
+++ client/src/components/TrackListingTableBody.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import { Table, Icon } from 'semantic-ui-react';
+import { Table, Icon, Transition } from 'semantic-ui-react';
 
 import TrackAlbum from './TrackAlbum';
 import DownloadTrack from './DownloadTrack';
@@ -11,18 +11,19 @@ const TrackListingTableBody = ({ trackListing, removeFromPlaylist }) => {
   if (Object.keys(trackListing).length > 0) {
     // sort by the order it was added to the playlist
     const orderedTracks = _.orderBy(trackListing, 'timeStamp', 'asc');
-    trackListingBody = _.map(orderedTracks, (track, idx)  => {
+
+    trackListingBody = _.map(orderedTracks, (track, idx) => {
       return (
         <Table.Row key={track.id}>
           <Table.Cell>
             {idx + 1}
           </Table.Cell>
           <Table.Cell>
-            <TrackAlbum              
-              imageUrl={track.images.large.secureUrl}              
+            <TrackAlbum
+              imageUrl={track.images.large.secureUrl}
               imageSize='tiny'
               iconSize='big'
-              track={track}              
+              track={track}
             />
           </Table.Cell>
           <Table.Cell>{track.title}</Table.Cell>
@@ -33,7 +34,7 @@ const TrackListingTableBody = ({ trackListing, removeFromPlaylist }) => {
           <Table.Cell><DownloadTrack track={track} mini blue /></Table.Cell>
           <Table.Cell>
             <Icon link name='delete' color='red' onClick={() => removeFromPlaylist(track.id.toString())} />
-            </Table.Cell>
+          </Table.Cell>
         </Table.Row>
       )
     });
@@ -45,7 +46,15 @@ const TrackListingTableBody = ({ trackListing, removeFromPlaylist }) => {
     )
   }
 
-  return <Table.Body>{trackListingBody}</Table.Body>;
+  return (
+    <Transition.Group 
+      as={Table.Body}
+      animation='slide right'
+      duration={200}
+    >
+      {trackListingBody}
+    </Transition.Group>
+  );
 };
 
 export default TrackListingTableBody;
