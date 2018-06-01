ca876b2f10b45dc2b5a306f409ae23790d59acb9
Jonathan Velez <jonathanvelez@gmail.com>
2018-05-31 22:22:42 -0700

    refactor to functional component

--- client/src/components/PlaylistDropdownControl.js
+++ client/src/components/PlaylistDropdownControl.js
@@ -4,39 +4,37 @@ import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { Dropdown } from 'semantic-ui-react';
 
-class PlaylistDropdownControl extends React.Component {
-  render() {
-    if (Object.keys(this.props.playlistList).length > 0) {
-      this.playlistList = _.map(this.props.playlistList, playlist => {
-        const { name, id } = playlist;
-        const url = `/playlist/${id}`;
+const PlaylistDropdownControl = ({ playlistList }) => {
+  if (Object.keys(playlistList).length > 0) {
+    this.playlistList = _.map(playlistList, playlist => {
+      const { name, id } = playlist;
+      const url = `/playlist/${id}`;
 
-        return (
-          <Dropdown.Item
-            text={name}
-            as={Link}
-            to={url}
-            key={id}
-            value={id}
-          />
-        )
-      });
-    } else {
-      this.playlistList = '';
-    }
-
-    return (
-      <Dropdown
-        item
-        scrolling
-        text='Playlists'
-      >
-        <Dropdown.Menu>
-          {this.playlistList}
-        </Dropdown.Menu>
-      </Dropdown>
-    )
+      return (
+        <Dropdown.Item
+          text={name}
+          as={Link}
+          to={url}
+          key={id}
+          value={id}
+        />
+      )
+    });
+  } else {
+    this.playlistList = '';
   }
+
+  return (
+    <Dropdown
+      item
+      scrolling
+      text='Playlists'
+    >
+      <Dropdown.Menu>
+        {this.playlistList}
+      </Dropdown.Menu>
+    </Dropdown>
+  )
 }
 
 const mapStateToProps = state => {
