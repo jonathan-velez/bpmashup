9b8df1f7a7fd7f43d3eeaf95d0c4407ccb621638
Jonathan Velez <jonathanvelez@gmail.com>
2018-06-12 21:26:59 -0700

    z-index fix

--- client/src/components/TrackActionDropdown.js
+++ client/src/components/TrackActionDropdown.js
@@ -3,12 +3,8 @@ import { Dropdown } from 'semantic-ui-react';
 
 const TrackActionDropdown = ({ downloadTrack, removeFromPlaylist, findSimilarTracks, track, ellipsisOrientation = 'vertical' }) => {
   return (
-    <Dropdown
-      icon={`ellipsis ${ellipsisOrientation}`}
-      floating
-      className='trackActionDropdown'
-    >
-      <Dropdown.Menu>
+    <Dropdown icon={`ellipsis ${ellipsisOrientation}`} floating>
+      <Dropdown.Menu className='trackActionDropdown'>
         <Dropdown.Item icon='download' text='Download' onClick={() => downloadTrack(track)} />
         <Dropdown.Item icon='delete' text='Delete' onClick={() => removeFromPlaylist(track.id.toString())} />
         <Dropdown.Item icon='search' text='Similar Tracks' onClick={() => findSimilarTracks(track.slug, track.id)} />
