675b4e6940037be729d025f1db073979c6a3b2dc
Jonathan Velez <jonathanvelez@gmail.com>
2018-06-12 20:24:06 -0700

    refactor to a stateless functional component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -15,11 +15,10 @@ import MutePlayer from './MutePlayer';
 import VolumeBar from './VolumeBar';
 import YouTubeButton from './YouTubeButton';
 
-class Footer extends Component {
+const Footer = ({ seekChange, seekMouseUp, seekMouseDown, playPause, setVolume, startAsync, getYoutubeLink, mediaPlayer, playerRef, loadTrack }) => {
 
   // TODO: Refactor these functions into a module
-  scrollToTrack = (trackId) => {
-    console.log('scrolling to', trackId)
+  this.scrollToTrack = (trackId) => {
     scroller.scrollTo(`track-${trackId}`, {
       duration: 750,
       delay: 50,
@@ -28,82 +27,80 @@ class Footer extends Component {
     });
   }
 
-  seekChange = e => {
-    this.props.seekChange(parseFloat(e.target.value));
+  this.seekChange = e => {
+    seekChange(+e.target.value);
   }
 
-  seekMouseUp = e => {
-    this.props.playerRef.seekTo(parseFloat(e.target.value))
-    this.props.seekMouseUp();
+  this.seekMouseUp = e => {
+    playerRef.seekTo(+e.target.value)
+    seekMouseUp();
   }
 
-  loadNextTrack = (incrementBy = 1) => {
-    this.props.loadTrack(getNextTrack(incrementBy));
+  this.loadNextTrack = (incrementBy = 1) => {
+    loadTrack(getNextTrack(incrementBy));
   }
 
-  getYouTube = loadedTrack => {
+  this.getYouTube = loadedTrack => {
     if (loadedTrack.id) {
-      this.props.startAsync();
-      this.props.getYoutubeLink(`${loadedTrack.artists[0].name} ${loadedTrack.title}`);
+      startAsync();
+      getYoutubeLink(`${loadedTrack.artists[0].name} ${loadedTrack.title}`);
     }
   }
 
-  render() {
-    const { playing, played, duration, loadedTrack } = this.props.mediaPlayer;
-    const youTubeUrl = _.get(this.props, 'mediaPlayer.youTubeObject.youTubeUrl');
+  const { playing, played, duration, loadedTrack } = mediaPlayer;
+  const youTubeUrl = _.get(this.props, 'mediaPlayer.youTubeObject.youTubeUrl');
 
-    return (
-      <Menu fixed='bottom' className='footer-menu' borderless>
-        <Container textAlign='center'>
-          <Menu.Item>
-            <Card>
-              <Card.Content>
-                <Card.Header>{loadedTrack.title}</Card.Header>
-                <Card.Meta>{constructLinks(loadedTrack.artists, 'artist')}</Card.Meta>
-              </Card.Content>
-            </Card>
-          </Menu.Item>
-          <Menu.Item>
-            <Image
-              src={loadedTrack.images && loadedTrack.images.large.secureUrl}
-              circular
-              size='tiny'
-              onClick={() => this.scrollToTrack(loadedTrack.id)}
-              className={`vinyl${playing ? ' vinyl-animate' : ''}`}
-            />
-          </Menu.Item>
-          <Menu.Item>
-            <YouTubeButton getYouTube={this.getYouTube} loadedTrack={loadedTrack} isLoaded={youTubeUrl ? true : false} />
-          </Menu.Item>
-          <Menu.Item>
-            <Duration seconds={duration * played} />
-          </Menu.Item>
-          <SeekBar
-            played={played}
-            seekMouseDown={this.props.seekMouseDown}
-            seekChange={this.seekChange}
-            seekMouseUp={this.seekMouseUp}
+  return (
+    <Menu fixed='bottom' className='footer-menu' borderless>
+      <Container textAlign='center'>
+        <Menu.Item>
+          <Card>
+            <Card.Content>
+              <Card.Header>{loadedTrack.title}</Card.Header>
+              <Card.Meta>{constructLinks(loadedTrack.artists, 'artist')}</Card.Meta>
+            </Card.Content>
+          </Card>
+        </Menu.Item>
+        <Menu.Item>
+          <Image
+            src={loadedTrack.images && loadedTrack.images.large.secureUrl}
+            circular
+            size='tiny'
+            onClick={() => this.scrollToTrack(loadedTrack.id)}
+            className={`vinyl${playing ? ' vinyl-animate' : ''}`}
           />
-          <Menu.Item>
-            <Duration seconds={duration} />
-          </Menu.Item>
-          <Menu.Item>
-            <PrevNextTrack buttonType='prev' handlePrevNextTrack={this.loadNextTrack} />
-          </Menu.Item>
-          <Menu.Item>
-            <PlayPauseButton isPlaying={playing} playPause={this.props.playPause} />
-          </Menu.Item>
-          <Menu.Item>
-            <PrevNextTrack buttonType='next' handlePrevNextTrack={this.loadNextTrack} />
-          </Menu.Item >
-          <Menu.Item>
-            <MutePlayer />
-            <VolumeBar volume={this.props.mediaPlayer.volume} setVolume={(e) => this.props.setVolume(parseFloat(e.target.value))} />
-          </Menu.Item>
-        </Container>
-      </Menu>
-    );
-  }
+        </Menu.Item>
+        <Menu.Item>
+          <YouTubeButton getYouTube={this.getYouTube} loadedTrack={loadedTrack} isLoaded={youTubeUrl ? true : false} />
+        </Menu.Item>
+        <Menu.Item>
+          <Duration seconds={duration * played} />
+        </Menu.Item>
+        <SeekBar
+          played={played}
+          seekMouseDown={seekMouseDown}
+          seekChange={this.seekChange}
+          seekMouseUp={this.seekMouseUp}
+        />
+        <Menu.Item>
+          <Duration seconds={duration} />
+        </Menu.Item>
+        <Menu.Item>
+          <PrevNextTrack buttonType='prev' handlePrevNextTrack={this.loadNextTrack} />
+        </Menu.Item>
+        <Menu.Item>
+          <PlayPauseButton isPlaying={playing} playPause={playPause} />
+        </Menu.Item>
+        <Menu.Item>
+          <PrevNextTrack buttonType='next' handlePrevNextTrack={this.loadNextTrack} />
+        </Menu.Item >
+        <Menu.Item>
+          <MutePlayer />
+          <VolumeBar volume={mediaPlayer.volume} setVolume={(e) => setVolume(+e.target.value)} />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
 }
 
 const mapStateToProps = state => {
