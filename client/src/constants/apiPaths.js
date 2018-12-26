c2db2e5bf11e87888419712080921001635d5581
Jonathan Velez <jonathanvelez@gmail.com>
2018-12-26 10:34:11 -0800

    use constant for search api path

--- client/src/constants/apiPaths.js
+++ client/src/constants/apiPaths.js
@@ -7,3 +7,4 @@ export const API_GET_YOUTUBE_LINK = `${API_ROOT}/youtube/search`;
 export const API_SIMILAR_TRACKS = `${API_ROOT}/tracks/similar`;
 export const API_GET_TRACKS = `${API_ROOT}/tracks`;
 export const API_GET_LOVED_LABELS = `${API_ROOT}/labels`;
+export const API_SEARCH_EVERYTHING = `${API_ROOT}/search`
--- client/src/thunks/searchEverythingThunk.js
+++ client/src/thunks/searchEverythingThunk.js
@@ -1,10 +1,11 @@
 import { SEARCH_EVERYTHING } from '../constants/actionTypes';
+import { API_SEARCH_EVERYTHING } from '../constants/apiPaths';
 import axios from 'axios';
 
 export const searchEverything = searchBy => {
   return (dispatch, getState) => {
 
-    const payload = axios.get(`/api/search?query=${searchBy}&perPage=50`);
+    const payload = axios.get(`${API_SEARCH_EVERYTHING}?query=${searchBy}&perPage=50`);
 
     dispatch({
       type: SEARCH_EVERYTHING,
