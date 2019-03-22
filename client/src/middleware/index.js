63f5d31b33cbf8fb6247c4913d9cd840dc8e841b
Jonathan Velez <jonathan.velez@vanguardlogistics.com>
2019-03-22 16:32:43 -0700

    addt'l de-normalized track play logs to firebase

--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -16,6 +16,58 @@ import {
 import { openLoginModalWindow } from '../actions/ActionCreators';
 
 export const activityLogger = store => next => action => {
+  const logTrack = (track, userId) => {
+    const { genres, artists, label, id } = track;
+
+    genres.forEach(genre => {
+      const genreRef = firebase.database().ref(`allTrackPlays/genres/${genre.slug}|${genre.id}`);
+      genreRef.transaction(currentValue => {
+        return (currentValue || 0) + 1;
+      });
+
+      if (userId) {
+        const genreRefUser = firebase.database().ref(`users/${userId}/trackPlays/genres/${genre.slug}|${genre.id}`);
+        genreRefUser.transaction(currentValue => {
+          return (currentValue || 0) + 1;
+        });
+      }
+    });
+
+    artists.forEach(artist => {
+      const artistRef = firebase.database().ref(`allTrackPlays/artists/${artist.slug}|${artist.id}`);
+      artistRef.transaction(currentValue => {
+        return (currentValue || 0) + 1;
+      });
+
+      if (userId) {
+        const artistRefUser = firebase.database().ref(`users/${userId}/trackPlays/artists/${artist.slug}|${artist.id}`);
+        artistRefUser.transaction(currentValue => {
+          return (currentValue || 0) + 1;
+        });
+      }
+    });
+
+    const labelRef = firebase.database().ref(`allTrackPlays/label/${label.slug}|${label.id}`);
+    labelRef.transaction(currentValue => {
+      return (currentValue || 0) + 1;
+    });
+
+    const labelRefUser = firebase.database().ref(`users/${userId}/trackPlays/label/${label.slug}|${label.id}`);
+    labelRefUser.transaction(currentValue => {
+      return (currentValue || 0) + 1;
+    });
+
+    const trackIdRef = firebase.database().ref(`allTrackPlays/tracks/${id}`);
+    trackIdRef.transaction(currentValue => {
+      return (currentValue || 0) + 1;
+    });
+
+    const trackIdRefUser = firebase.database().ref(`users/${userId}/trackPlays/tracks/${id}`);
+    trackIdRefUser.transaction(currentValue => {
+      return (currentValue || 0) + 1;
+    });
+  }
+
   const id = v4();
   const state = store.getState();
 
@@ -34,6 +86,8 @@ export const activityLogger = store => next => action => {
         track: action.payload,
         timeStamp: moment().format(),
       });
+
+      logTrack(action.payload, userId); // TODO use a setTimeout or something to delay the execution of the log until after 5 seconds of play time.
       break;
     case GET_YOUTUBE_LINK:
       firebase.set(`/trackPlaysYouTube/${id}`, {
