ac05a15505f9ff546710c0f35df6ba6775a2f096
Jonathan Velez <jonathanvelez@gmail.com>
2019-04-11 19:32:25 -0700

    Add link to all tracks, menu for releases & tracks

--- client/src/components/Label.js
+++ client/src/components/Label.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Grid, Image, Header } from 'semantic-ui-react';
+import { Grid, Image, Header, Menu, Button } from 'semantic-ui-react';
 import Scroll from 'react-scroll';
+import { Link } from 'react-router-dom';
 
 import { getLabelDetail } from '../thunks';
 import LoveItem from './LoveItem';
@@ -12,6 +13,10 @@ import ItemCards from './ItemCards';
 import TrackListingGroup from './TrackListingGroup';
 
 class Label extends Component {
+  state = {
+    activeItem: 'releases',
+  }
+
   componentDidMount() {
     this.fetchLabelDetails();
   }
@@ -27,11 +32,30 @@ class Label extends Component {
     this.props.getLabelDetail(this.props.match.params.labelId);
   }
 
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+  }
+
   render() {
-    const { labelDetail, trackListing } = this.props;
+    const { labelDetail, trackListing, location } = this.props;
+    const { activeItem } = this.state;
     const { labelData, releasesData } = labelDetail;
     const { images, name, id, biography, genres } = labelData;
     const imageSrc = images && images.large.secureUrl;
+    const { pathname } = location;
+
+    let activeItemContent = null;
+    switch (activeItem) {
+      case 'releases':
+        activeItemContent =
+          releasesData &&
+          <ItemCards items={releasesData} itemType='release' showHeader={false} />
+        break;
+      case 'tracks':
+        activeItemContent =
+          trackListing &&
+          <TrackListingGroup trackListing={trackListing} />
+    }
 
     return (
       <Fragment>
@@ -59,12 +83,30 @@ class Label extends Component {
               </Grid.Column>
             </Grid.Row>
           }
-          {releasesData &&
-            <ItemCards items={releasesData} itemType='release' />
-          }
         </Grid>
-        <Header textAlign='left' dividing>TRACKS</Header>
-        <TrackListingGroup trackListing={trackListing} />
+        <Menu secondary pointing>
+          <Menu.Item
+            link
+            name='releases'
+            className='item-header'
+            active={activeItem === 'releases'}
+            onClick={this.handleItemClick}
+          >Featured Releases</Menu.Item>
+          <Menu.Item
+            link
+            name='tracks'
+            className='item-header'
+            active={activeItem === 'tracks'}
+            onClick={this.handleItemClick}
+          >Top 10 Tracks</Menu.Item>
+          <Menu.Item position='right'>
+            <Button as={Link} to={`${pathname}/tracks`}>
+              View All Tracks
+            </Button>
+          </Menu.Item>
+        </Menu>
+        {activeItemContent}
+
       </Fragment>
     );
   }
