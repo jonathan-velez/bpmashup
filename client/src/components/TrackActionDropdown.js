060c8bef5a5271976011818c870213bb8c0a48b6
Jonathan Velez <jonathanvelez@gmail.com>
2019-04-19 16:04:12 -0700

    user permissions

--- client/src/components/TrackActionDropdown.js
+++ client/src/components/TrackActionDropdown.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 import AddToPlaylist from './AddToPlaylist';
 import DownloadTrack from './DownloadTrack';
 import SimilarTrack from './SimilarTrack';
 import LoveItem from './LoveItem';
 
-const TrackActionDropdown = ({ track, ellipsisOrientation = 'vertical', upward = false }) => {
+const TrackActionDropdown = ({ track, ellipsisOrientation = 'vertical', upward = false, userDetail }) => {
+  const { permissions } = userDetail;
+  const canZip = Array.isArray(permissions) && permissions.includes('zipZip');
+
   return (
     <Dropdown icon={`ellipsis ${ellipsisOrientation}`} floating upward={upward}>
       <Dropdown.Menu className='trackActionDropdown'>
-        <DownloadTrack type='dropdownItem' track={track} />
+        {canZip && <DownloadTrack type='dropdownItem' track={track} />}
         <SimilarTrack type='dropdownItem' track={track} />
         <AddToPlaylist type='dropdownItem' track={track} />
         <LoveItem type='dropdownItem' itemType='track' item={track} />
@@ -19,4 +23,10 @@ const TrackActionDropdown = ({ track, ellipsisOrientation = 'vertical', upward =
   );
 };
 
-export default TrackActionDropdown;
+const mapStateToProps = state => {
+  return {
+    userDetail: state.userDetail,
+  }
+}
+
+export default connect(mapStateToProps, null)(TrackActionDropdown);
