3626b0c27383119f4256b8adede00e3038fef2fb
Jonathan Velez <jonathanvelez@gmail.com>
2019-09-04 20:28:35 -0700

    wrap header in segment

--- client/src/components/ChartListingController.js
+++ client/src/components/ChartListingController.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
-import { Transition, Grid, Image, Header } from 'semantic-ui-react';
+import { Transition, Grid, Image, Header, Segment } from 'semantic-ui-react';
 
 import ResponsiveTrackListing from './ResponsiveTrackListing';
 import GenreLabel from './GenreLabel';
@@ -39,34 +39,37 @@ class ChartListingController extends Component {
 
     return (
       <React.Fragment>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={4}>
-              <Image src={secureUrl} />
-            </Grid.Column>
-            <Grid.Column width={8} textAlign='left'>
-              <Header as='h1' style={trackTitleHeader}>
-                {name}
-                {chartOwnerName && <Header.Subheader>Owner: {chartOwnerName}</Header.Subheader>}
-                {publishDate && <Header.Subheader>{`Publish Date:  ${publishDate}`}</Header.Subheader>}
-              </Header>
-              {genres && genres.map((genre, idx) => {
-                return (
-                  <GenreLabel key={idx} genreName={genre.name} genreSlug={genre.slug} genreId={genre.id} />
-                )
-              })}
-              <p>{description}</p>
-            </Grid.Column>
-          </Grid.Row>
-          {
-            tracks.length > 0 ?
-              <Transition visible={visible} animation='fade' duration={1500}>
-                <ResponsiveTrackListing trackListing={tracks} isPlaylist={false} isLoading={isLoading} page={1} perPage={50} />
-              </Transition>
-              :
-              null
-          }
-        </Grid>
+        <Segment placeholder padded='very'>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={4}>
+                <Image src={secureUrl} />
+              </Grid.Column>
+              <Grid.Column width={8} textAlign='left'>
+                <Header as='h1' style={trackTitleHeader}>
+                  {name}
+                  {chartOwnerName && <Header.Subheader>Owner: {chartOwnerName}</Header.Subheader>}
+                  {publishDate && <Header.Subheader>{`Publish Date:  ${publishDate}`}</Header.Subheader>}
+                </Header>
+                {genres && genres.map((genre, idx) => {
+                  return (
+                    <GenreLabel key={idx} genreName={genre.name} genreSlug={genre.slug} genreId={genre.id} />
+                  )
+                })}
+                <p>{description}</p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        {
+          tracks.length > 0 ?
+            <Transition visible={visible} animation='fade' duration={1500}>
+              <ResponsiveTrackListing trackListing={tracks} isPlaylist={false} isLoading={isLoading} page={1} perPage={50} />
+            </Transition>
+            :
+            null
+        }
+
       </React.Fragment>
     );
   }
