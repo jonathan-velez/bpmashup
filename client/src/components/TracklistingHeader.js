135f9b929224897ffcef73c8253e49b9a169c54a
Jonathan Velez <jonathanvelez@gmail.com>
2019-11-14 19:33:02 -0800

    handle null headerTitle case

--- client/src/components/TracklistingHeader.js
+++ client/src/components/TracklistingHeader.js
@@ -15,7 +15,7 @@ class TracklistingHeader extends React.PureComponent {
     return (
       <Header size='huge' className='tracklistHeader' textAlign='left' dividing>
         {headerPrefix ? <span className='tracklistHeaderPrefix'>{headerPrefix} </span> : null}
-        {deslugify(headerTitle.toUpperCase())} {headerType === 'artist' || headerType === 'label' ? <LoveItem itemType={item} type={headerType} item='button' /> : null}
+        {deslugify(headerTitle && headerTitle.toUpperCase())} {headerType === 'artist' || headerType === 'label' ? <LoveItem itemType={item} type={headerType} item='button' /> : null}
       </Header>
     );
   }
