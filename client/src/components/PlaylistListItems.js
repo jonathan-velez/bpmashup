189e78a8728330c27345d5c26541c36d198399d9
Jonathan Velez <jonathanvelez@gmail.com>
2019-12-03 16:26:43 -0800

    missing key on playlist items

--- client/src/components/PlaylistListItems.js
+++ client/src/components/PlaylistListItems.js
@@ -4,14 +4,18 @@ import { List, Checkbox } from 'semantic-ui-react';
 const PlaylistListItems = ({ playlistList, track, addToPlaylist }) => {
   const playlistItems = playlistList.map(playlist => {
     const added = playlist.listOfTracks && playlist.listOfTracks.includes(track.id);
-    const { tracks = {} } = playlist;
+    const { tracks = {}, id: playlistId } = playlist;
 
     const listItemStyle = {
       cursor: 'pointer',
     }
 
     return (
-      <List.Item onClick={() => addToPlaylist({ id: playlist.id, added })} style={listItemStyle}>
+      <List.Item
+        onClick={() => addToPlaylist({ id: playlistId, added })}
+        style={listItemStyle}
+        key={playlistId}
+      >
         <List.Content floated='right'>
           <Checkbox checked={added} />
         </List.Content>
