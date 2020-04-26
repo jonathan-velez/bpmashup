f8d4d7afbfdd29da16e2b92edd80d7dbc5f7b6b1
Jonathan Velez <jonathanvelez@gmail.com>
2020-04-26 10:27:15 -0700

    refactor Tracklisting load from Label page

--- client/src/thunks/labelThunk.js
+++ client/src/thunks/labelThunk.js
@@ -1,43 +1,57 @@
 import { callAPIorCache } from '../seessionStorageCache';
-import { API_GET_LABEL_BY_ID, API_MOST_POPULAR_BY_LABEL, API_MOST_POPULAR_RELEASES_BY_LABEL } from '../constants/apiPaths';
-import { START_ASYNC, GET_LABEL_DETAIL, LOAD_TRACKS } from '../constants/actionTypes';
+import {
+  API_GET_LABEL_BY_ID,
+  API_MOST_POPULAR_BY_LABEL,
+  API_MOST_POPULAR_RELEASES_BY_LABEL,
+} from '../constants/apiPaths';
+import {
+  START_ASYNC,
+  GET_LABEL_DETAIL,
+  FETCH_TRACKS,
+} from '../constants/actionTypes';
+import { DEFAULT_PER_PAGE } from '../constants/defaults';
 
 export const getLabelDetail = (labelId, labelName) => {
-  return async dispatch => {
+  return async (dispatch) => {
     dispatch({
-      type: START_ASYNC
+      type: START_ASYNC,
     });
 
-    const labelDataCall = callAPIorCache(`${API_GET_LABEL_BY_ID}?id=${labelId}&name=${labelName}`);
-    const labelTracksCall = callAPIorCache(`${API_MOST_POPULAR_BY_LABEL}?id=${labelId}&perPage=25`);
-    const labelReleasesCall = callAPIorCache(`${API_MOST_POPULAR_RELEASES_BY_LABEL}?id=${labelId}`);
+    const labelDataCall = callAPIorCache(
+      `${API_GET_LABEL_BY_ID}?id=${labelId}&name=${labelName}`,
+    );
+    const labelTracksCall = callAPIorCache(
+      `${API_MOST_POPULAR_BY_LABEL}?id=${labelId}&perPage=${DEFAULT_PER_PAGE}`,
+    );
+    const labelReleasesCall = callAPIorCache(
+      `${API_MOST_POPULAR_RELEASES_BY_LABEL}?id=${labelId}`,
+    );
 
     const labelData = await labelDataCall;
 
     const { data } = labelData;
     let labelDataPayload = {};
-    
+
     if (data.results.length > 0) {
       labelDataPayload = data.results[0];
     }
-    
+
     const labelTracks = await labelTracksCall;
-    const tracksData = labelTracks.data.results;
-    
+
     const labelReleases = await labelReleasesCall;
     const releasesData = labelReleases.data.results;
 
     dispatch({
-      type: LOAD_TRACKS,
-      payload: tracksData,
-    })
+      type: FETCH_TRACKS,
+      payload: labelTracks,
+    });
 
     dispatch({
       type: GET_LABEL_DETAIL,
       payload: {
         labelData: labelDataPayload,
         releasesData,
-      }
+      },
     });
-  }
-}
+  };
+};
