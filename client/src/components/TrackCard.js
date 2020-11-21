c8142a3d1f6b68deb865d0d333ce0f1aeb251256
Jonathan Velez <jonathanvelez@gmail.com>
2020-11-21 08:48:29 -0800

    add release data and key to track card

--- client/src/components/TrackCard.js
+++ client/src/components/TrackCard.js
@@ -5,39 +5,71 @@ import { connect } from 'react-redux';
 
 import TrackAlbum from './TrackAlbum';
 import TrackCardActionRow from './TrackCardActionRow';
-import { constructLinks, trackGenreColors, constructTrackLink } from '../utils/trackUtils';
+import {
+  constructLinks,
+  trackGenreColors,
+  constructTrackLink,
+} from '../utils/trackUtils';
+import { musicalKeyFilter } from '../utils/helpers';
 import { hasZippyPermission } from '../selectors';
 
 const TrackCard = ({ track, showPosition = true, canZip }) => {
+  const {
+    id,
+    genres = [],
+    images = {},
+    position,
+    artists = [],
+    label = {},
+    releaseDate,
+    key,
+  } = track;
   return (
     <Card
       className='flex-card'
-      id={`track-${track.id}`}
-      color={trackGenreColors[track.genres[0].name.toLowerCase()]}
+      id={`track-${id}`}
+      color={trackGenreColors[genres[0].name.toLowerCase()]}
       raised
     >
       <TrackAlbum
-        imageUrl={track.images.large.secureUrl}
+        imageUrl={images.large && images.large.secureUrl}
         track={track}
       />
       <Card.Content>
-        {track.position && showPosition && <Label attached='top left' color='grey' ribbon>{track.position}</Label>}
-        <Card.Header className='track-title'>{constructTrackLink(track)}</Card.Header>
-        <Card.Content>{constructLinks(track.artists, 'artist')}</Card.Content>
-        <Card.Content><Link to={`/label/${track.label.slug}/${track.label.id}`}>[{track.label.name}]</Link></Card.Content>
-        <Card.Content>{constructLinks(track.genres, 'genre')}</Card.Content>
+        {position && showPosition && (
+          <Label attached='top left' color='grey' ribbon>
+            {position}
+          </Label>
+        )}
+        <Card.Header className='track-title'>
+          {constructTrackLink(track)}
+        </Card.Header>
+        <Card.Content>{constructLinks(artists, 'artist')}</Card.Content>
+        <Card.Content>
+          <Link to={`/label/${label.slug}/${label.id}`}>[{label.name}]</Link>
+        </Card.Content>
+        <Card.Content>{constructLinks(genres, 'genre')}</Card.Content>
+        <Card.Content>{releaseDate}</Card.Content>
+        <Card.Content>{musicalKeyFilter(key && key.shortName)}</Card.Content>
       </Card.Content>
       <Card.Content extra>
-        <TrackCardActionRow canZip={canZip} numOfButtons={'three'} track={track} />
+        <TrackCardActionRow
+          canZip={canZip}
+          numOfButtons={'three'}
+          track={track}
+        />
       </Card.Content>
-    </Card >
-  )
-}
+    </Card>
+  );
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     canZip: hasZippyPermission(state),
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, null)(TrackCard);
+export default connect(
+  mapStateToProps,
+  null,
+)(TrackCard);
