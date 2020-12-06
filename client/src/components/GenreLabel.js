7de79f3d058e5fdc27449d3483a21de98d42f67f
Jonathan Velez <jonathanvelez@gmail.com>
2020-12-05 18:34:27 -0800

    fix genre url

--- client/src/components/GenreLabel.js
+++ client/src/components/GenreLabel.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Label } from 'semantic-ui-react';
 
 const GenreLabel = ({ genreName, genreSlug, genreId }) => {
-  const url = `/most-popular/genre/${genreSlug}/${genreId}/`;
+  const url = `/genre/${genreSlug}/${genreId}/`;
   return (
     <Label as={Link} to={url} className='genre-label' color='grey'>
       {genreName}
